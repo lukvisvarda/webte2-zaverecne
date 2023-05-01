71a58fea27018d99b86f11832f4d513e883617d3
sdrlicka <sdrlicka@gmail.com>
2023-05-01 14:09:49 +0200

    frontend editor working

---
+++ frontend/components/editor/MathEditor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <math-field :config="config" :value="value" @input="handleChange"></math-field>
+    <p v-html="rendered"></p>
+  </div>
+</template>
+
+<script>
+import { MathField, convertLatexToMarkup } from 'mathlive'
+
+export default {
+  components: {
+    MathField
+  },
+
+  props: {
+    onValueChange: {
+      type: Function,
+      default: () => {},
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      config: {
+        virtualKeyboardMode: 'onfocus'
+      },
+      value: 'f(x) = ',
+      rendered: ''
+    }
+  },
+
+  methods:{
+    handleChange(event) {
+      this.value = event.target.value;
+    }
+  },
+
+  watch: {
+    value(newLatex) {
+      console.log("WATCHING")
+      this.rendered = convertLatexToMarkup(newLatex);
+      this.onValueChange(newLatex);
+    }
+  }
+}
+</script>
