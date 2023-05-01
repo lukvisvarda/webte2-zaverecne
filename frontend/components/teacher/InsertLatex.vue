2a68afa10ab59b4ce69f7f7bf62ee2362601c0d1
sdrlicka <sdrlicka@gmail.com>
2023-05-01 12:23:39 +0200

    table reload fix

--- frontend/components/teacher/InsertLatex.vue
+++ frontend/components/teacher/InsertLatex.vue
@@ -10,16 +10,15 @@
           </div>
           <button type="submit">Submit</button>
         </form>
-
       </div>
-      <TeacherTable/>
+      <TeacherTable :rows="rows"/>
   </div>
 </template>
 
 <script>
 
 import api from "../../utils/api";
-import {LATEX_POST} from "../../constants/edpoints";
+import {LATEX_GET, LATEX_POST} from "../../constants/edpoints";
 import TeacherTable from "./TeacherTable.vue";
 import AssignThesis from "./AssignThesis.vue";
 export default {
@@ -28,7 +27,38 @@ export default {
     TeacherTable,
     AssignThesis
   },
+
+  data(){
+    return {
+      rows: [],
+    }
+  },
+
+  mounted() {
+    this.fetchData();
+  },
   methods: {
+    fetchData() {
+      api.get(LATEX_GET)
+        .then(response => {
+          //loop through the response and push the data to the rows array
+          for (let i = 0; i < response.length; i++) {
+            // loop over parsed data and push to rows
+            for(let j = 0; j < response[i].parsed.length; j++){
+              this.rows.push({
+                name: response[i].parsed[j].name,
+                task: response[i].parsed[j].task,
+                // solution: response[i].parsed[j].solution,
+                solution: response[i].parsed[j].solution,
+                file_name: response[i].name
+              });
+            }
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
     submitForm() {
       const fileInput = document.querySelector('#latex-file');
       const file = fileInput.files[0];
@@ -43,13 +73,14 @@ export default {
       })
         .then(response => {
           console.log('Success:', response);
+          this.fetchData();
         })
         .catch(error => {
           // Handle error
           console.error('Error:', error);
         });
       //reload page
-      location.reload();
+      // location.reload();
     }
   }
 }
